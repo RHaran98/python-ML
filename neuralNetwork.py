c7af7c0c8446bd74433b7ddbc00f73fcf87429ec
Haran Ravindran <RHaran98@users.noreply.github.com>
2017-08-19 23:10:59 +0530

    Update neuralNetwork.py
    First working commit. Added relu activation instead of tanh

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -2,15 +2,15 @@ import numpy as np
 import tensorflow as tf
 import sklearn.datasets as datasets
 from matplotlib import pyplot as plt
-
+from sklearn.metrics import accuracy_score as score
 class neuralNetwork:
     def __init__(self,x,y,no_of_classes,no_hidden_layers = 3, learning_rate = 0.01, neurons_per_layer = 5):
         self.data_x = x
         self.data_y = y
         self.no_of_input = len(x[0])
         self.no_of_classes = no_of_classes
-        self.x = tf.placeholder("float", [None, self.no_of_input])
-        self.y = tf.placeholder("float", [None, self.no_of_classes])
+        self.x = tf.placeholder("float", shape=[None, self.no_of_input])
+        self.y = tf.placeholder("float", shape=[None, self.no_of_classes])
         self.no_hidden_layers = no_hidden_layers
         self.neurons_per_layer = neurons_per_layer
         self.learning_rate = learning_rate
@@ -26,18 +26,19 @@ class neuralNetwork:
         for i in range(1,self.no_hidden_layers+1):
             self.weights['layer'+str(i)] = tf.Variable(tf.random_normal([self.neurons_per_layer, self.neurons_per_layer]))
             self.biases['layer' + str(i)] = tf.Variable(tf.random_normal([ self.neurons_per_layer]))
-        self.weights['output'] = tf.Variable(tf.random_normal([ self.neurons_per_layer]))
+        self.weights['output'] = tf.Variable(tf.random_normal([ self.neurons_per_layer,self.no_of_classes]))
         self.biases['output'] = tf.Variable(tf.random_normal([ self.no_of_classes]))
 
     def define_graph(self):
         self.output = self.x         #Input
-        for i in range(self.no_hidden_layers+1):
-            self.output = tf.add(tf.matmul(self.output,self.weights['layer'+str(i)]),self.biases['layer'+str(i)])
-            self.output = tf.tanh(self.output)
-        self.output = tf.matmul(self.output,tf.expand_dims(self.weights['output'],1))+self.biases['output']
+        with tf.device('/gpu:0'):
+            for i in range(self.no_hidden_layers+1):
+                self.output = tf.add(tf.matmul(self.output,self.weights['layer'+str(i)]),self.biases['layer'+str(i)])
+                self.output = tf.nn.relu(self.output)
+            self.output = tf.matmul(self.output, self.weights['output'])+self.biases['output']
 
-        self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.output, labels=self.y))
-        self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.cost)
+            self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.output, labels=self.y))
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.cost)
 
     def train(self,epochs):
         self.init_weights()
@@ -46,19 +47,13 @@ class neuralNetwork:
         with tf.Session() as sess:
             sess.run(init)
             for i in range(epochs):
-                for point,label in zip(self.data_x,self.data_y):
-                    _, c = sess.run([self.optimizer, self.cost], feed_dict={self.x: point, self.y: label})
+                #for point,label in zip(self.data_x,self.data_y):
+                _, c = sess.run([self.optimizer, self.cost], feed_dict={self.x: self.data_x, self.y: self.data_x})
                 if i % 100 == 0:
-                    print "Cost : " + str(c)
-
-
-
-
-np.random.seed(0)
-X, y = datasets.make_moons(200, noise=0.20)
-plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
-#plt.show()
-print "Shape X : ",X.shape
-print "Shape Y : ",y.shape
-nn = neuralNetwork(X,y,2)
-nn.train(500)
+                    print "Epoch : {0} Cost : {1}\n".format(i+1,c)
+    def pred(self,x):
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            pred = sess.run(self.output,feed_dict={ self.x:x})
+            pred_args = sess.run(tf.arg_max(pred,dimension=1))
+        return pred,pred_args
